b19c706b94fa3e41c5da42845466458065db397f
Stephen Baldwin <stephen.baldwin@myhominc.com>
2018-12-26 16:50:56 -0700

    added data

--- app/index.js
+++ app/index.js
@@ -11,7 +11,29 @@ function initTabs(elems) {
             }
         }
         this.classList.add("active");
+        if(this.dataset.info) getData(this.dataset.info);
     }
+}
+function getData(data) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "/data/" + data, true);
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if(this.readyState === 4 && this.status === 200) showInfo(this.response);
+    }
+}
+function showInfo(data) {
+    const names = document.getElementsByClassName("name"),
+    info = JSON.parse(data),
+    keys = Object.keys(info),
+    len = keys.length;
+    let i = 1;
+    for(; i < len; i++) {
+        document.getElementById(keys[i]).textContent = info[keys[i]];
+    }
+    i = names.length;
+    while(i--) names[i].textContent = info.name + " Project";
+    
 }
 initTabs("item");
 initTabs("tab");
--- server.js
+++ server.js
@@ -1,5 +1,59 @@
 const express = require('express'),
-path = require('path'),
-server = express();
+server = express(), 
+users = [
+    {
+        project: "12345",
+        name: "John Smitherson",
+        address: "1701 Main Street",
+        province: "Denver, CO 80101",
+        year: "1996",
+        phone: "(303)555-5555",
+        email: "johnsmitherson@email.com",
+        store: "1502",
+        net: "$1,562.24",
+        margin: "41.70%",
+        poItems: "9",
+        woItems: "4",
+        poTotal: "$3,762.97",
+        woTotal: "$3,762.97"
+    }, 
+    {
+        project: "218718",
+        name: "Bryce Anderson",
+        address: "1234 A Street",
+        province: "Denver, CO 80237",
+        year: "2009",
+        phone: "(303)123-1234",
+        email: "bryceanderson@email.com",
+        store: "1650",
+        net: "$1,262.12",
+        margin: "63.01%",
+        poItems: "6",
+        woItems: "2",
+        poTotal: "$7,899.00",
+        woTotal: "$7,001.20"
+    },
+    {
+        project: "927346",
+        name: "idk blah",
+        address: "1 idk Street",
+        province: "Denver, CO 80237",
+        year: "2012",
+        phone: "(303)987-6543",
+        email: "idk@email.com",
+        store: "1111",
+        net: "$1,111.11",
+        margin: "11.11%",
+        poItems: "7",
+        woItems: "3",
+        poTotal: "$1,111.11",
+        woTotal: "$1,111.11"
+    }
+];
 server.use(express.static("./app"));
+server.get("/data/" + "*", (req, res)=> {
+    const url = req.url.split("/")[2];
+    let i = users.length;
+    while(i--) if(users[i].project === url) return res.send(users[i]);
+});
 server.listen(process.env.PORT || 8080, console.log("App Running"));
